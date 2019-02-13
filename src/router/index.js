bd4c74f348e5019231dbfbb7e1da717495105bdf
zhy <1226079584@qq.com>
2019-02-13 17:26:14 +0800

    登录组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,20 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import login from '@/components/login.vue'
+import home from '@/components/home.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: ''
+      name: 'login',
+      path: '/login',
+      component: login
+    },
+    {
+      name: 'home',
+      path: '/home',
+      component: home
     }
   ]
 })
